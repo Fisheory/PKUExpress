b55b329fc45e5c19df59d4c33d4aeadef55b6ed3
sfs <965371392@qq.com>
2024-11-13 00:07:19 +0800

    完成task部分视图

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('task_list/<int:st>/<int:en>', views.task_list),
-    path('<int:pk>/task_detail/', views.task_detail),
+    path('', views.task_list),
+    path('task-list/', views.task_list), # default st=0, en=10
+    path('task-list/<int:st>/<int:en>/', views.task_list),
+    path('<int:pk>/task-detail/', views.task_detail),
+    path('task-create/', views.task_create),
+    path('<int:pk>/task-accept/', views.task_accept),
+    path('task-finish/', views.task_finish),
 ]
