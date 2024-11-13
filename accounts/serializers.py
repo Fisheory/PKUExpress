6009c6ea60cc5f4b7e0227723dad62bd0c3df89b
leason-lyx <13640486755@139.com>
2024-11-14 04:53:36 +0800

    更新了user的序列器

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,16 +6,17 @@ class CustomUserSerializer(serializers.ModelSerializer):
     
     password = serializers.CharField(write_only=True)
     
-    # 下面会通过两个函数自定义其展示方法
-    published_tasks = serializers.SerializerMethodField()
-    accepted_tasks = serializers.SerializerMethodField()
+    published_tasks = serializers.SerializerMethodField() # 用于返回用户发布的任务
+    accepted_tasks = serializers.SerializerMethodField()  # 用于返回用户接受的任务
+    accepted_accepted_tasks = serializers.SerializerMethodField() # 用于返回用户接受的任务中已经被接取的任务
+    accepted_finished_tasks = serializers.SerializerMethodField() # 用于返回用户接受的任务中已经被完成的任务
     
     # Todo: finished_tasks?
     # 如果添加, 应该可以仿造accepted_tasks, 记得在fields添加
     
     class Meta:   
         model = CustomUser
-        fields = ['username', 'password', 'email', 'phone', 'gold', 'published_tasks', 'accepted_tasks']
+        fields = ['username', 'password', 'email', 'phone', 'gold', 'published_tasks', 'accepted_tasks', 'finished_tasks']
         
     def create(self, validated_data):
         password = validated_data.pop('password', None)
@@ -32,4 +33,13 @@ class CustomUserSerializer(serializers.ModelSerializer):
     def get_accepted_tasks(self, obj):
         tasks = obj.accepted_tasks.all()
         return TaskSerializer(tasks, many=True).data
+    
+    def get_accepted_accepted_tasks(self, obj):
+        tasks = obj.accepted_tasks.filter(status='accepted')
+        return TaskSerializer(tasks, many=True).data
+    
+    def get_accepted_finished_tasks(self, obj):
+        tasks = obj.accepted_tasks.filter(status='finished')
+        return TaskSerializer(tasks, many=True).data
+        
     
