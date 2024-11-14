5c126c38d87800136499dfb5bcd8807fcd1df099
leason-lyx <13640486755@139.com>
2024-11-15 04:35:46 +0800

    更正了一些错误

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     # GET tasks: 列出所有任务
     # POST tasks: 创建一个新任务
-    path('', views.TaskList), 
+    path('', views.TaskList.as_view()), 
 
     # GET tasks/<int:pk>: 获取一个任务的详细信息
     # PUT tasks/<int:pk>: 更新一个任务
-    path('<int:pk>', views.TaskDetail),
+    path('<int:pk>', views.TaskDetail.as_view()),
 ]
