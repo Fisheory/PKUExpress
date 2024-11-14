06ab2c79d5e7b08d8b992d9eed991a5f7bda68f5
sfs <965371392@qq.com>
2024-11-14 23:59:02 +0800

    accounts/test.py对目前的用户模型的测试

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
     phone = models.CharField(max_length=11, blank=True)
-    gold = models.IntegerField(default=0)
+    # 给予每个用户1000金币, 方便后续测试
+    gold = models.IntegerField(default=1000)
     
     def __str__(self):
         return self.username
