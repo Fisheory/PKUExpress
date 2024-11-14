83d5271660b992939d503ec737e8ec7824868a30
sfs <965371392@qq.com>
2024-11-14 16:28:26 +0800

    修复bug，views更新，model和serializer更新

--- tasks/views.py
+++ tasks/views.py
@@ -11,19 +11,18 @@ import json
 # Create your views here.
 @csrf_exempt
 @api_view(['GET'])
-def task_list(request, st=0, en=10):
-    '''
-    Todo: 现在只是按task的id来索引, 实际情况下id可能会跳号等等, 而且按id会优先展示时间最靠前的
-          后续修改为按照时间排序, 进一步多种排序方法
-    
+def task_list(request):
+    ''' 
     用户状态: 未登录或已登录
-    简介: 任务列表视图，返回任务列表
+    简介: 任务列表视图, 返回按发布时间倒序的任务列表
+    注意: 参数通过GET请求传递, 例如/tasks/task-list/?st=0&en=10
+          未提供参数时默认返回前10个任务
     
-    params: st=start index, en=end index
+    params: st=start index, (optional)
+            en=end index    (optional)
     URL: /tasks/
          /tasks/task-list/
          (default st=0, en=10)
-         /tasks/task-list/<int:st>/<int:en>/
     Method: GET
     Header: Content-Type: application/json, Authorization: Token token (optional)
     Body: JSON {}
@@ -39,15 +38,27 @@ def task_list(request, st=0, en=10):
                     "publisher": "publisher",
                     "worker": "worker",                     (可能缺失, 表示未被接受)
                     "create_time": "create_time",
-                    "update_time": "update_time",
+                    "update_time": "update_time",           (值为最后一次修改时间, 可能是状态变更)
+                    "finish_time": "finish_time",           (值可能为null, 表示未完成)
                     "deadline": "deadline",                 (时间格式均为"YYYY-MM-DDTHH:MM:SS+08:00",
                                                             例如"2024-11-12T15:05:41.480925+08:00")
                     "status": "status"                      (值为to_be_accepted, accepted, finished, out_of_date)
                  }
-        Error: Code 404 JSON {'status': 'error', 'msg': 'task is not exist'}
+        Error: Code 400 JSON {'status': 'error', 'msg': '...'}
                Code 405: Method Not Allowed
     '''
-    tasks = Task.objects.all()[st:en]
+    st = request.GET.get('st')
+    en = request.GET.get('en')
+    if st is not None and en is not None:
+        try:
+            st = int(st)
+            en = int(en)
+        except ValueError:
+            return Response(status=400)
+    else:
+        st = 0
+        en = 10
+    tasks = Task.objects.order_by('-create_time')[st:en]
     # 每次调用时检查这些任务是否过期
     for task in tasks:
         task.out_of_date()
@@ -55,28 +66,38 @@ def task_list(request, st=0, en=10):
     return Response(serializer.data)
     
 @csrf_exempt
-@api_view(['GET', 'PUT', 'DELETE'])
-def task_detail(request, pk):
+@api_view(['GET'])
+def task_detail(request):
     '''
     用户状态: 未登录或已登录
-    简介：任务详情视图，返回任务详情
+    简介: 任务详情视图, 返回特定id的任务详情
+    注意: 参数通过GET请求传递, 例如/tasks/task-detail/?pk=1
     
     params: pk=task id
-    URL: /tasks/<int:pk>/task-detail/
+    URL: /tasks/task-detail/
     Method: GET
     Header: Content-Type: application/json, Authorization: Token token (optional)
     Body: JSON {}
     Response: 
         Success: Code 200 JSON {task}
                  task介绍同上
-        Error: Code 404 JSON {'status': 'error', 'msg': 'task is not exist'}
+        Error: Code 400 JSON {'status': 'error', 'msg': 'error message'}
                Code 405: Method Not Allowed
     
     '''
+    pk = request.GET.get('pk')
+    if pk is not None:
+        try:
+            pk = int(pk)
+        except ValueError as e:
+            return Response({'status': 'error', 'msg': str(e)}, status=400)
+    else:
+        return Response({'status': 'error', 'msg': 'need pk'}, status=400)
+    
     try:
         task = Task.objects.get(pk=pk)
-    except Task.DoesNotExist:
-        return Response(status=404)
+    except Task.DoesNotExist as e:
+        return Response({'status': 'error', 'msg': str(e)}, status=400)
     
     if request.method == 'GET':
         serializer = TaskSerializer(task)
@@ -120,34 +141,43 @@ def task_create(request):
     serializer = TaskSerializer(data=data, context={'request': request})
     if serializer.is_valid():
         serializer.save()
-        return Response(status=201)
+        return Response(serializer.data, status=201)
     else:
         return Response(serializer.errors, status=400)
     
 @csrf_exempt
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
-def task_accept(request, pk):
+def task_accept(request):
     '''
     用户状态: 已登录
     简介: 接受任务视图, 接受特定任务
+    注意: 通过POST表单传递参数, key=pk, value=task id
     
-    params: pk=task id
-    URL: /tasks/<int:pk>/task-accept/
+    URL: /tasks/task-accept/
     Method: POST
-    Header: Content-Type: application/json, Authorization: Token token
-    Body: JSON {}
+    Header: Content-Type: application/x-www-form-urlencoded, Authorization: Token token
+    Body: key=pk, value=task id
     Response:
         Success: Code 200 JSON {'status': 'success', 'msg': 'accept success'}
         Error: Code 400 JSON {'status': 'error', 'msg': 'error message'}
                Code 405: Method Not
     
     '''
+    pk = request.POST.get('pk')
+    if pk is not None:
+        try:
+            pk = int(pk)
+        except ValueError as e:
+            return Response({'status': 'error', 'msg': str(e)}, status=400)
+    else:
+        return Response({'status': 'error', 'msg': 'need pk'}, status=400)
+    
     user = request.user
     try:
         task = Task.objects.get(pk=pk)
     except Task.DoesNotExist:
-        return Response({'status': 'error', 'msg': 'task is not exist'}, status=404)
+        return Response({'status': 'error', 'msg': 'task is not exist'}, status=400)
     
     try:
         # 调用task.accept()方法, 合法性检查在方法内部
@@ -162,26 +192,34 @@ def task_accept(request, pk):
 def task_finish(request):
     '''
     用户状态: 已登录
-    简介: 完成任务视图, 完成用户当前接受的任务
-    注意: 无需传入参数, 完成的是该用户当前接受的任务, 目前未检查任务完成条件
+    简介: 完成任务视图, 完成用户当前接受的任务pk
+    注意: 通过POST表单传递参数, key=pk, value=task id
     
     URL: /tasks/task-finish/
     Method: POST
-    Header: Content-Type: application/json, Authorization: Token token
-    Body: JSON {}
+    Header: Content-Type: application/x-www-form-urlencoded, Authorization: Token token
+    Body: key=pk, value=task id
     Response:
         Success: Code 200 JSON {'status': 'success', 'msg': 'finish success'}
         Error: Code 400 JSON {'status': 'error', 'msg': 'error message'}
                Code 405: Method Not Allowed
     '''
     
-    # 选择通过当前用户的accepted_tasks来获取任务, 如果是用pk岂不是可以修改别人的任务了
+    # 选择通过当前用户的accepted_tasks来获取任务, 并查询是否存在任务pk
+    pk = request.POST.get('pk')
+    if pk is not None:
+        try:
+            pk = int(pk)
+        except ValueError as e:
+            return Response({'status': 'error', 'msg': str(e)}, status=400)
+    else:
+        return Response({'status': 'error', 'msg': 'need pk'}, status=400)
+    
     user = request.user
     try:
-        # .first()是因为假定只能接受一个任务
-        task = user.accepted_tasks.first()
+        task = user.accepted_tasks.get(pk=pk)
     except Task.DoesNotExist:
-        return Response({'status': 'error', 'msg': 'have not accepted task'}, status=404)
+        return Response({'status': 'error', 'msg': 'have not accepted this task'}, status=400)
     
     try:
         task.finish()
