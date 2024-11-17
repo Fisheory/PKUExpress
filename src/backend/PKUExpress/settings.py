20bdcc26ee57ed4c634531a5b1a8d7f8af818450
leason-lyx <13640486755@139.com>
2024-11-17 17:33:17 +0800

    添加了服务器设置

--- src/backend/PKUExpress/settings.py
+++ src/backend/PKUExpress/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6^z91-wp=9psiqv!j+@@k@e@duc%hnri8(cqwum9b&kg3oyezw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["123.56.18.162"]
 
 
 # Application definition
