688e33e0ad8264f3318048f628044a39918c321c
sfs <965371392@qq.com>
2024-11-25 21:46:54 +0800

    修bug

--- src/backend/accounts/urls.py
+++ src/backend/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('auth/register', views.UserRegister.as_view()),
+    path('auth/register/', views.UserRegister.as_view()),
     path('auth/verification-code/', views.VerificationCode.as_view()),
     path('auth/reset-password/', views.UserResetPassword.as_view()),
     path('auth/login/', views.UserLogin.as_view()),
