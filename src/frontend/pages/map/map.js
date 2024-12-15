c7598caa7129e218b2c6da83c1f09ff7549a4a2b
Corina Dong <127618772+cd0903@users.noreply.github.com>
2024-12-15 23:22:01 +0800

    Changed to 高德地图

--- src/frontend/pages/map/map.js
+++ src/frontend/pages/map/map.js
@@ -1,66 +1,129 @@
 // pages/map/map.js
+var amapFile = require('../../libs/amap-wx.130.js');
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    subKey: 'FGNBZ-FZKLV-NTKPS-5Y3AK-YQCB3-LOFL3',
-    enable3d: false,
-    showLocation: true,
-    showCompass: false,
-    enableOverlooking: false,
-    enableZoom: true,
-    enableScroll: true,
-    enableRotate: false,
-    drawPolygon: false,
-    enableSatellite: false,
-    enableTraffic: false,
-    latitude: '39.9976',
-    longitude: '116.3102',
     markers: [],
-    circles: [],
-    polylines: [],
-    polygons: [],
-    showDialog: false,
-    currentMarker: null,
-    features: [
-      {"name":"地点1","geometry":{"type":"Point","coordinates":[116.3102,39.9976]}},
-    ]
+    latitude: '',
+    longitude: '',
+    textData: {},
+    inputLocation: '',
+    selectedLocation: null // To store confirmed location
   },
 
-  onReady: function () {
-    const map = wx.createMapContext("map");
-    map.moveToLocation();
-  },
-
-  onShow: function () {
-    const markers = this.data.features.map((feature,index) => {
-      return {
-        id: index,
-        latitude: feature.geometry.coordinates[1],
-        longitude: feature.geometry.coordinates[0],
-        properties: {
-          name: feature.name,
-          code: feature.code
-        },
+  onLoad: function () {
+    var that = this;
+    this.mapContext = new amapFile.AMapWX({ key: 'd351e5acebebe211388ac1efb1e18540' });
+  
+    // Get current location and display on map
+    this.mapContext.getRegeo({
+      success: function (data) {
+        if (data && data.length > 0) {
+          var location = data[0];
+  
+          // Set default marker for current location
+          var marker = [{
+            id: 0,
+            latitude: location.latitude,
+            longitude: location.longitude,
+            width: 22,
+            height: 32
+          }];
+  
+          that.setData({
+            markers: marker,
+            latitude: location.latitude,
+            longitude: location.longitude,
+            textData: {
+              name: location.name || '当前位置',
+              desc: location.desc || '您当前的位置'
+            },
+            selectedLocation: {
+              latitude: location.latitude,
+              longitude: location.longitude,
+              name: location.name || '当前位置'
+            }
+          });
+        }
+      },
+      fail: function () {
+        wx.showToast({ title: '无法获取当前位置', icon: 'none' });
       }
     });
-    
+  },
+  
+
+  // Update input value
+  onInputChange: function (e) {
     this.setData({
-      markers: markers
+      inputLocation: e.detail.value
     });
   },
 
+  // Search for location
+  searchLocation: function () {
+    var that = this;
+    var input = this.data.inputLocation;
 
-  handleMarkerTap(e) {
-    console.log(e);
-    const marker = this.data.markers.find(item => item.id == e.markerId);
-    marker && this.setData({
-      currentMarker: marker,
-      showDialog: true
+    if (!input) {
+      wx.showToast({ title: '请输入地点名称', icon: 'none' });
+      return;
+    }
+
+    that.mapContext.getInputtips({
+      keywords: input,
+      success: function (data) {
+        if (data && data.tips && data.tips.length > 0) {
+          var location = data.tips[0].location.split(',');
+
+          var latitude = parseFloat(location[1]);
+          var longitude = parseFloat(location[0]);
+
+          // Update markers and center map
+          var marker = [{
+            id: 0,
+            latitude: latitude,
+            longitude: longitude,
+            iconPath: "../../img/marker.png",
+            width: 22,
+            height: 32
+          }];
+
+          that.setData({
+            markers: marker,
+            latitude: latitude,
+            longitude: longitude,
+            textData: {
+              name: data.tips[0].name,
+              desc: data.tips[0].district
+            },
+            selectedLocation: {
+              latitude: latitude,
+              longitude: longitude,
+              name: data.tips[0].name
+            }
+          });
+        } else {
+          wx.showToast({ title: '未找到相关地点', icon: 'none' });
+        }
+      },
+      fail: function () {
+        wx.showToast({ title: '搜索失败', icon: 'none' });
+      }
     });
-    //this.data.showDialog = true;
   },
 
-})
+  // Confirm the selected location
+  confirmLocation: function () {
+    if (this.data.selectedLocation) {
+      wx.showModal({
+        title: '选择成功！',
+        content: `您已选择： ${this.data.selectedLocation.name}`,
+        showCancel: false
+      });
+      console.log('Confirmed location:', this.data.selectedLocation);
+    } else {
+      wx.showToast({ title: 'Please search and select a location first.', icon: 'none' });
+    }
+  }
+});
